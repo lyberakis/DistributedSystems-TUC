5c4be67aec4238303135756664b46b1d67baa6ed
lyberakis <vasilis@lyberakis.gr>
2020-04-28 13:58:58 +0300

    pieces images added

--- games/react-chess/src/pieces/king.js
+++ games/react-chess/src/pieces/king.js
@@ -2,7 +2,7 @@ import Piece from './piece.js';
 
 export default class King extends Piece {
   constructor(player){
-    super(player, (player === 1? "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg"));
+    super(player, (player === 1? require("../images/white_king.svg") : require("../images/black_king.svg")));
   }
 
   isMovePossible(src, dest){
