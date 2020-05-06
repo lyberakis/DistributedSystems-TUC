285b3ed7829114fc3269cf81ce75e00cbfc98571
pgiannakopoulos <pgiannakopoulos397@gmail.com>
2020-05-06 22:02:56 +0300

    zookeeper for gamemaster

--- containers/GameMaster/src/zookeeper.py
+++ containers/GameMaster/src/zookeeper.py
@@ -1,10 +1,10 @@
 from kazoo.client import KazooClient
 import time
+from utils import log_settings
+import logging as log
 
 zk = KazooClient(hosts='zookeeper:2181')
 zk.start()
-
-from kazoo.client import KazooState
 
 def my_listener(state):
 	if state == KazooState.LOST:
@@ -23,14 +23,19 @@ zk.ensure_path("/games")
 # Create a node with data
 zk.create("/games/playmaster", None)
 
+playmasters = []
+pos = 0
+
 @zk.ChildrenWatch("/games/playmaster")
 def watch_children(children):
-	print("Children are now: %s" % children)
+	# log.info(f'Watch -- Children are now: {children}')
+	global playmasters
+	playmasters = children.copy();
 
-	for child in children:
-		# Print the version of a node and its data
-		data, stat = zk.get("/games/playmaster/"+child)
-		print("Version: %s, data: %s" % (stat.version, data.decode("utf-8")))
+	# for child in children:
+	# 	# Print the version of a node and its data
+	# 	data, stat = zk.get("/games/playmaster/"+child)
+	# 	print("Version: %s, data: %s" % (stat.version, data.decode("utf-8")))
 
 		
 # Above function called immediately, and from then on
@@ -39,5 +44,19 @@ def watch_children(children):
 # def watch_node(data, stat):
 #     print("Version: %s, data: %s" % (stat.version, data.decode("utf-8")))
 
+def assign():
+	if pos >= len(playmasters):
+		pos = 0
+
+	seletected = playmasters[pos]
+
+	return pos;
+
 while True:
 	time.sleep(1)
+	# log.info(f'Children are now: {playmasters}')
+	# for child in playmasters:
+	# 	# Print the version of a node and its data
+	# 	data, stat = zk.get("/games/playmaster/"+child)
+	# 	log.info(f'Version {stat.version} data: {data.decode("utf-8")}')
+
